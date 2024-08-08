01189f10bd64d400558a638c9d7a3b99be678b42
Tatiana Mikhailova <imagetata@gmail.com>
2024-08-08 22:51:32 +0200

    moons list with scroll for each planet

--- planet.js
+++ planet.js
@@ -1,32 +1,31 @@
 // Anna, Michael
 function createPlanetInfo(selectedPlanet) {
-  const planetInfoContainer = document.querySelector('.planet-info'); 
-  planetInfoContainer.innerHTML = `
-      <div>
-          <h1 class="planet-name">${selectedPlanet.name}</h1>
-          <h2 class="planet-name-latin">${selectedPlanet.latinName}</h2>
-      </div>
-      <p class="planet-description">${selectedPlanet.desc}</p>
-      <article class="planet-data">
-          <section class="info-left">
-              <h5>OMKRETS </h5>
-              <p class="omkrets-data">${selectedPlanet.circumference}</p><br>
-              <h5>MAX TEMPERATUR</h5>
-              <p class="max-temperature">${selectedPlanet.temp.day}</p><br>
-              <h5>MÅNAR</h5>
-                  <ul>
-                      <li>${selectedPlanet.moons[0]}</li>
-                  </ul>
-          </section>
-          <section class="info-right">
-              <h5>KM FRÅN SOLEN</h5>
-              <p class="distance-from-sun">${selectedPlanet.distance}</p><br>
-              <h5>MIN TEMPERATUR</h5>
-              <p class="min-temperature">${selectedPlanet.temp.night}</p><br>
-              <button id="add-favorite-btn">lägg till som favorit</button>
-          </section>                    
-      </article>
-  `;  
+    const planetInfoContainer = document.querySelector('.planet-info');
+    planetInfoContainer.innerHTML = `
+        <div>
+            <h1 class="planet-name">${selectedPlanet.name}</h1>
+            <h2 class="planet-name-latin">${selectedPlanet.latinName}</h2>
+        </div>
+        <p class="planet-description">${selectedPlanet.desc}</p>
+        <article class="planet-data">
+            <section class="info-left">
+                <h5>OMKRETS </h5>
+                <p class="omkrets-data">${selectedPlanet.circumference}</p><br>
+                <h5>MAX TEMPERATUR</h5>
+                <p class="max-temperature">${selectedPlanet.temp.day}</p><br>
+                <h5>MÅNAR</h5>
+                    <ul id="moons"></ul>
+            </section>
+            <section class="info-right">
+                <h5>KM FRÅN SOLEN</h5>
+                <p class="distance-from-sun">${selectedPlanet.distance}</p><br>
+                <h5>MIN TEMPERATUR</h5>
+                <p class="min-temperature">${selectedPlanet.temp.night}</p><br>
+                <button id="add-favorite-btn">lägg till som favorit</button>
+            </section>                    
+        </article>
+    `;
+    addMoonsList(selectedPlanet);
 };
 
 // Anna
@@ -52,4 +51,21 @@ function submitPlanetToFavorites(planet) {
     btn.addEventListener("click", () => {
         addPlanetToFavorites(planet);
     });
+};
+
+// Tatiana
+function addMoonsList(planet) {
+    let moonsList = planet.moons;
+    let ulRef = document.querySelector('#moons');;
+    if (moonsList.length > 0) {
+        moonsList.forEach(moon => {
+            let li = document.createElement('li');
+            li.innerHTML = moon;
+            ulRef.appendChild(li);   
+        });
+    } else {
+        let li = document.createElement('li');
+        li.innerHTML = '-';
+        ulRef.appendChild(li); 
+    }
 };
