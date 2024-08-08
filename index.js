2d1e76fda2eef9bee427f4991d36c2fd5099c37b
Tatiana Mikhailova <imagetata@gmail.com>
2024-08-08 15:23:07 +0200

    send till single planet with info and started to separate js-files

--- index.js
+++ index.js
@@ -1,15 +1,55 @@
-// See the fetch in hidden_key.js locally
-
-// Alla
-// Window addeventlistener function
 window.addEventListener("load", async () => {
   let key = fetchPlanetsApiKey();
   let planetList = await fetchPlanets(key);
   storePlanets(planetList);
   changePageHeader();
-  let planetName = choosePlanet();
-  createPlanetInfo(planetName); // do toLowerCase() before comparing
+
+  choosePlanet();
+  let selectedPlanetId = localStorage.getItem('selectedPlanetId');
+  let selectedPlanet = getStoredPlanetData(selectedPlanetId);
+
+  createPlanetInfo(selectedPlanet);
+
+  localStorage.clear();
 });
+
+// Louise, Tatiana
+// when clicking on planet, redirect from home page to single planet page
+function choosePlanet() {
+  let planetNamesList = document.querySelectorAll(".planet");    
+  planetNamesList.forEach((planet, i) => {
+      planet.addEventListener("click", () => { 
+          localStorage.setItem('selectedPlanetId', i);     
+          window.open("planet.html", "_self");
+      });       
+  });
+}
+
+// Alla
+function getStoredPlanetData(index) {
+  let planets = JSON.parse(localStorage.getItem('planetList'));
+  return planets[index];
+  // const storedData = localStorage.getItem("planetList");
+  // return storedData ? JSON.parse(storedData[index]) : [];
+}
+
+
+
+// Tatiana, Louise
+// Hover effect with header's name changing
+function changePageHeader() {
+  let planetNames = document.querySelectorAll(".planet"); // Array
+  let h1Ref = document.querySelector("h1");
+
+  planetNames.forEach((planet) => {
+    planet.addEventListener("mouseover", () => {
+      h1Ref.innerHTML = `${planet.id.toString()}`;
+    });
+    planet.addEventListener("mouseout", () => {
+      h1Ref.innerHTML = "Solsystemet";
+    });
+  });
+}
 
 // Alla
 // Fetch data using the key
@@ -29,35 +69,8 @@ function storePlanets(planetList) {
   localStorage.setItem("planetList", JSON.stringify(planetList));
 }
 
-// Tatiana, Louise
-// Hover effect with header's name changing
-function changePageHeader() {
-  let planetNames = document.querySelectorAll(".planet"); // Array
-  let h1Ref = document.querySelector("h1");
 
-  planetNames.forEach((planet) => {
-    planet.addEventListener("mouseover", () => {
-      h1Ref.innerHTML = `${planet.id.toString()}`;
-    });
-    planet.addEventListener("mouseout", () => {
-      h1Ref.innerHTML = "Solsystemet";
-    });
-  });
-}
 
-// Louise, Tatiana
-// when clicking on planet, redirect from home page to single planet page
-function choosePlanet() {
-  let planetNamesList = document.querySelectorAll(".planet");
-  let planetName;
-  planetNamesList.forEach((planet) => {
-    planet.addEventListener("click", () => {
-      planetName = `${planet.id.toString()}`;
-      window.open("planet.html", "_self");
-      return planetName;
-    });
-  });
-}
 
 // TODO
 // Create temlpate for a planet
--- planet.js
+++ planet.js
@@ -1,80 +1,30 @@
-// Anna och Michael
-
-//--------------------------------------------------------------------------------------------------
-//--------------------------------------------------------------------------------------------------
-// Retrieve planet data from localStorage
-// Waiting to clarify with the other group on how they fetched the selectedPlanet
-// Leaving this function as our version for now
-function getStoredPlanetData() {
-    const storedData = localStorage.getItem('planetList');
-    return storedData ? JSON.parse(storedData) : [];
-}
-const planetList = getStoredPlanetData();
-// console.log(planetList[4]);
-// console.log(planetList);
-//--------------------------------------------------------------------------------------------------
-//--------------------------------------------------------------------------------------------------
-
-
-
-// Anna och Michael
-// Displays Planet Data
+// Anna, Michael
 function createPlanetInfo(selectedPlanet) {
-
-//--------------------------------------------------------------------------------------------------
-//--------------------------------------------------------------------------------------------------
-    // we will get this variable from the click function later
-    selectedPlanet = planetList[1]
-    // console.log('selectedPlanet', selectedPlanet);
-//--------------------------------------------------------------------------------------------------
-//--------------------------------------------------------------------------------------------------
-
-
-    const planetInfoContainer = document.querySelector('.planet-info');
-    planetInfoContainer.innerHTML = '';
-
-    console.log(planetInfoContainer.textContent);
-
-    const planetInfo = document.querySelector('.planet-info');
-
-
-
-
-    //  Populate the HTML elements with the planet data.
-    planetInfoContainer.innerHTML = `
-
-                <div>
-                    <h1 class="planet-name">${selectedPlanet.name}</h1>
-                    
-                    <h2 class="planet-name-latin">${selectedPlanet.latinName}</h2>
-                </div>
-                <p class="planet-description">${selectedPlanet.desc}</p>
-                <article class="planet-data">
-                    <section class="info-left">
-                        <h5>OMKRETS </h5>
-                        <p class="omkrets-data">${selectedPlanet.circumference}</p><br>
-                        <h5>MAX TEMPERATUR</h5>
-                        <p class="max-temperature">${selectedPlanet.temp.day}</p><br>
-                        <h5>MÅNAR</h5>
-                        <ul>
-                            <li>Månen</li>
-                        </ul>
-                    </section>
-                    <section class="info-right">
-                        <h5>KM FRÅN SOLEN</h5>
-                        <p class="distance-from-sun">${selectedPlanet.distance}</p><br>
-                        <h5>MIN TEMPERATUR</h5>
-                        <p class="min-temperature">${selectedPlanet.temp.night}</p><br>
-                        <button id="add-favorite-btn">lägg till som favorit</button>
-                    </section>                    
-                </article>
-
-    `
-}
-
-//--------------------------------------------------------------------------------------------------
-//--------------------------------------------------------------------------------------------------
-//this will be an event click
-createPlanetInfo();
-//--------------------------------------------------------------------------------------------------
-//--------------------------------------------------------------------------------------------------
+  const planetInfoContainer = document.querySelector('.planet-info'); 
+  planetInfoContainer.innerHTML = `
+      <div>
+          <h1 class="planet-name">${selectedPlanet.name}</h1>
+          <h2 class="planet-name-latin">${selectedPlanet.latinName}</h2>
+      </div>
+      <p class="planet-description">${selectedPlanet.desc}</p>
+      <article class="planet-data">
+          <section class="info-left">
+              <h5>OMKRETS </h5>
+              <p class="omkrets-data">${selectedPlanet.circumference}</p><br>
+              <h5>MAX TEMPERATUR</h5>
+              <p class="max-temperature">${selectedPlanet.temp.day}</p><br>
+              <h5>MÅNAR</h5>
+                  <ul>
+                      <li>${selectedPlanet.moons[0]}</li>
+                  </ul>
+          </section>
+          <section class="info-right">
+              <h5>KM FRÅN SOLEN</h5>
+              <p class="distance-from-sun">${selectedPlanet.distance}</p><br>
+              <h5>MIN TEMPERATUR</h5>
+              <p class="min-temperature">${selectedPlanet.temp.night}</p><br>
+              <button id="add-favorite-btn">lägg till som favorit</button>
+          </section>                    
+      </article>
+  `;  
+};
