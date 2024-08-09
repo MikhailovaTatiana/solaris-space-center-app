f90293e3ee75dc913f4c03a635e5dd0805d80875
AG <anna.gabain@gmail.com>
2024-08-09 12:00:40 +0200

    Set the background color of .active-planet to match the displayed planet color

--- planet.js
+++ planet.js
@@ -1,7 +1,11 @@
 // Anna, Michael
 function createPlanetInfo(selectedPlanet) {
-    const planetInfoContainer = document.querySelector('.planet-info');
-    planetInfoContainer.innerHTML = `
+  const planetInfoContainer = document.querySelector(".planet-info");
+
+  const activePlanetClassName = `${selectedPlanet.name.toLowerCase()}`;
+  console.log(activePlanetClassName);
+
+  planetInfoContainer.innerHTML = `
         <div>
             <h1 class="planet-name">${selectedPlanet.name}</h1>
             <h2 class="planet-name-latin">${selectedPlanet.latinName}</h2>
@@ -25,47 +29,89 @@ function createPlanetInfo(selectedPlanet) {
             </section>                    
         </article>
     `;
-    addMoonsList(selectedPlanet);
-};
+
+  // Update the left side half circle based on the planet name
+  updatePlanetColor(selectedPlanet.name.toLowerCase());
+
+  addMoonsList(selectedPlanet);
+}
+
+
+
+function updatePlanetColor(planetId) {
+  const planetElement = document.querySelector(".active-planet");
+
+  planetElement.classList.remove(
+    "sun-color",
+    "mercury-color",
+    "venus-color",
+    "earth-color",
+    "mars-color",
+    "jupiter-color",
+    "saturn-color"
+  );
+
+  // Add the new planet color class to the left side half circle
+  if (planetId === "solen") {
+    planetElement.style.backgroundColor = "var(--sun-color)";
+  } else if (planetId === "merkurius") {
+    planetElement.style.backgroundColor = "var(--mercury-color)";
+  } else if (planetId === "venus") {
+    planetElement.style.backgroundColor = "var(--venus-color)";
+  } else if (planetId === "jorden") {
+    planetElement.style.backgroundColor = "var(--earth-color)";
+  } else if (planetId === "mars") {
+    planetElement.style.backgroundColor = "var(--mars-color)";
+  } else if (planetId === "jupiter") {
+    planetElement.style.backgroundColor = "var(--jupiter-color)";
+  } else if (planetId === "saturnus") {
+    planetElement.style.backgroundColor = "var(--saturn-color)";
+  } else if (planetId === "neptunus") {
+    planetElement.style.backgroundColor = "var(--neptune-color)";
+  } else if (planetId === "uranus") {
+    planetElement.style.backgroundColor = "var(--uranus-color)";
+  } else {
+    planetElement.style.backgroundColor = "var(--sun-color)";
+  }
+}
 
 // Anna
 function addPlanetToFavorites(planet) {
-    let favorites = JSON.parse(localStorage.getItem("favorites")) || [];
-    
-    // Check if planet is already in favorites
-    if (!favorites.find(fav => fav.name === planet.name)) {
-        favorites.push(planet);
-        localStorage.setItem("favorites", JSON.stringify(favorites));
-        // alert(`${planet.name} has been added to favorites.`);
-        window.location.href = 'favorites.html'
+  let favorites = JSON.parse(localStorage.getItem("favorites")) || [];
 
-    } else {
-        alert(`${planet.name} is already in favorites.`);
-        // window.location.href = 'favorites.html'
-    }
-};
+  // Check if planet is already in favorites
+  if (!favorites.find((fav) => fav.name === planet.name)) {
+    favorites.push(planet);
+    localStorage.setItem("favorites", JSON.stringify(favorites));
+    // alert(`${planet.name} has been added to favorites.`);
+    window.location.href = "favorites.html";
+  } else {
+    alert(`${planet.name} is already in favorites.`);
+    window.location.href = 'favorites.html'
+  }
+}
 
 // Tatiana
 function submitPlanetToFavorites(planet) {
-    let btn = document.querySelector('#add-favorite-btn');
-    btn.addEventListener("click", () => {
-        addPlanetToFavorites(planet);
-    });
-};
+  let btn = document.querySelector("#add-favorite-btn");
+  btn.addEventListener("click", () => {
+    addPlanetToFavorites(planet);
+  });
+}
 
 // Tatiana
 function addMoonsList(planet) {
-    let moonsList = planet.moons;
-    let ulRef = document.querySelector('#moons');;
-    if (moonsList.length > 0) {
-        moonsList.forEach(moon => {
-            let li = document.createElement('li');
-            li.innerHTML = moon;
-            ulRef.appendChild(li);   
-        });
-    } else {
-        let li = document.createElement('li');
-        li.innerHTML = '-';
-        ulRef.appendChild(li); 
-    }
-};
+  let moonsList = planet.moons;
+  let ulRef = document.querySelector("#moons");
+  if (moonsList.length > 0) {
+    moonsList.forEach((moon) => {
+      let li = document.createElement("li");
+      li.innerHTML = moon;
+      ulRef.appendChild(li);
+    });
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = "-";
+    ulRef.appendChild(li);
+  }
+}
